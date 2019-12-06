d1f6bd34466ea8dec816b3fb7120710da82d1cd4
Dariy Pozinenko <33901469+dpozinen@users.noreply.github.com>
2019-12-06 17:33:23 +0200

    uri needs tests

--- UriBuilder.java
+++ UriBuilder.java
@@ -1,79 +1,199 @@
-public final class UriWrapper {
-    private Uri uri;
+package blackbee.swarm.parsinghelper;
 
-    public UriWrapper(String url) {
-        this.uri = new Uri(url);
-    }
+import blackbee.common.collections.generic.KeyValuePair;
+import blackbee.swarm.core.web.IUriSegment;
+import blackbee.swarm.core.web.Uri;
+import blackbee.swarm.core.web.UriPathSegment;
+import org.apache.commons.lang3.StringUtils;
 
-    public UriWrapper(Uri uri) {
-        this.uri = new Uri(uri);
-    }
+import java.util.ArrayList;
+import java.util.List;
 
-    public UriWrapper(String url, String host) {
-        this.uri = new Uri(url, host);
-    }
+/**
+ * Wrapper for {@link Uri} built on the fluent design pattern.
+ * Introduces some additional features and safety measures that the {@link Uri} class lacks.
+ * @author dpozinen
+ */
+public final class UriWrapper
+{
+	private Uri uri;
 
-    public UriWrapper add(String k, String v) {
-        this.uri.addParameter(k, v);
-        return this;
-    }
+	public UriWrapper(String url) {
+		//noinspection deprecation
+		uri = new Uri(url);
+	}
 
-    public UriWrapper set(String k, String v) {
-        if (this.containsKey(k))
-            this.uri.setParameter(k, v);
-        return this;
-    }
+	public UriWrapper(Uri uri) {
+		this.uri = new Uri(uri);
+	}
 
-    public UriWrapper remove(String k, String v) {
-        // todo
-        return this;
-    }
+	public UriWrapper(String url, String host) {
+		uri = new Uri(url, host);
+	}
 
-    public UriWrapper clear() {
-        this.uri.clearParameters();
-        return this;
-    }
+	public UriWrapper add(String k, String v) {
+		uri.addParameter(k, v);
+		return this;
+	}
 
-    public UriWrapper containsSegment(String k) {
-        // todo
-        return this;
-    }
+	public UriWrapper add(String k, int v) {
+		return add(k, String.valueOf(v));
+	}
 
-    public UriWrapper containsKey(String k) {
-        // todo
-        return this;
-    }
+	public UriWrapper set(String k, String v) {
+		if (containsKey(k))
+			uri.setParameter(k, v);
+		return this;
+	}
 
-    public UriWrapper containsValue(String v) {
-        // todo
-        return this;
-    }
+	public UriWrapper set(String k, int v) {
+		return set(k, String.valueOf(v));
+	}
 
-    public UriWrapper https() {
-        this.uri.setProtocol("https");
-        return this;
-    }
+	public UriWrapper setOrAdd(String k, String v) {
+		if ( goodKV(k, v) ) {
+			if ( containsKey(k) )
+				uri.setParameter(k, v);
+			else
+				uri.addParameter(k, v);
+		}
+		return this;
+	}
 
-    public UriWrapper http() {
-        this.uri.setProtocol("http");
-        return this;
-    }
+	public UriWrapper setOrAdd(String k, int v) {
+		return setOrAdd(k, String.valueOf(v));
+	}
 
-    public UriWrapper encode() {
-        this.uri = this.uri.setEncodingEnabled(true);
-        return this;
-    }
+	public UriWrapper removeKey(String k) {
+		if (containsKey(k))
+			uri.removeParameter(k);
+		return this;
+	}
 
-    public UriWrapper decode() {
-        this.uri = this.uri.setEncodingEnabled(false);
-        return this;
-    }
+	public UriWrapper removeValue(int v) {
+		return removeValue(String.valueOf(v));
+	}
 
-    public Uri uri() {
-        return this.uri;
-    }
+	public UriWrapper removeValue(String v) {
+		String k = null;
+		if ( containsValue(v) )
+			for ( KeyValuePair<String, String> p : uri.getParameters() )
+				if ( p.getValue().equals(v) )
+					k = p.getKey();
 
-    public String toString() {
-        return this.uri.toString();
-    }
+		if ( k != null )
+			uri.removeParameter(k);
+		return this;
+	}
+
+	public UriWrapper clearParams() {
+		uri.clearParameters();
+		return this;
+	}
+
+	public UriWrapper clearAll() {
+		uri.removePathSegments(uri.getUriPathSegments());
+		return clearParams();
+	}
+
+	public boolean containsSegment(String s) {
+		for ( IUriSegment segment : uri.getUriPathSegments() )
+			if ( segment.toString().replace("/", "").equals(s) )
+				return true;
+		return false;
+	}
+
+	public boolean containsKey(String k) {
+		return uriParametersContains(k, true);
+	}
+
+	public boolean containsValue(String v) {
+		return uriParametersContains(v, false);
+	}
+
+	public boolean keyIsValue(String k, String v) {
+		return goodKV(k, v) && containsKey(k) && uri.getParameter(k).equals(v);
+	}
+
+	public UriWrapper https() {
+		uri.setProtocol("https://");
+		return this;
+	}
+
+	public UriWrapper http() {
+		uri.setProtocol("http://");
+		return this;
+	}
+
+	public UriWrapper encode() {
+		uri = uri.setEncodeEnabled(true);
+		return this;
+	}
+
+	public UriWrapper decode() {
+		uri = uri.setEncodeEnabled(false);
+		return this;
+	}
+
+	public Uri uri() {
+		return uri;
+	}
+
+	@Override
+	public String toString() {
+		return uri.toString();
+	}
+
+	private boolean uriParametersContains(String s, boolean comparingKeys) {
+		for ( KeyValuePair<String, String> parameter : uri.getParameters() ) {
+			if ( comparingKeys && parameter.getKey().equals(s) )
+				return true;
+			else if ( !comparingKeys && parameter.getValue().equals(s) )
+				return true;
+		}
+		return false;
+	}
+
+	private boolean goodKV(String k, String v) {
+		return StringUtils.isNotBlank(k) && v != null;
+	}
+
+	public UriWrapper removeSegment(String s) {
+		return containsSegment(s) ? removeSegmentInternal(s) : this;
+	}
+
+	public UriWrapper removeSegmentsMatches(String regex) {
+		List<String> toDelete = new ArrayList<>();
+		for ( IUriSegment segment : uri.getUriPathSegments() )
+			if ( segment.toString().matches(regex) )
+				toDelete.add(segment.toString());
+
+		for ( String d : toDelete )
+			removeSegment(d);
+		return this;
+	}
+
+	private UriWrapper removeSegmentInternal(String s) {
+		uri.getUriPathSegments().remove(new UriPathSegment(s));
+		return this;
+	}
+
+	public UriWrapper removeValueMatches(String regex) {
+		return this;
+	}
+
+	public UriWrapper removeKeyMatches(String regex) {
+		return this;
+	}
+
+	private UriWrapper removeParamMatches()
+	{
+//		for ( KeyValuePair<String, String> p : uri.getParameters() )
+//			if ( p.getValue().equals(v) )
+//				k = p.getKey();
+//
+//		if ( k != null )
+//			uri.removeParameter(k);
+		return this;
+	}
 }
