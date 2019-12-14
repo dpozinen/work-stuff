ff512f1ea0803ea1b753deb5e49b3bb72fbd06c8
dpozinen <dpozinen@gmail.com>
2019-12-14 14:44:51 +0200

    add docks and prevent setting if notRankingMode

--- src/main/java/work/stuff/templates/RankingHelper.java
+++ src/main/java/work/stuff/templates/RankingHelper.java
@@ -2,20 +2,48 @@ package main.java.work.stuff.templates;
 
 import java.io.Serializable;
 
+/**
+ * A ranking helper, that contains {@link #overviewUrl} and {@link #rank} with setters and getters for them.
+ *
+ * Designed to be inherited by helpers that have require ranking mode functionality.
+ * Uses the simulated self type idiom to provide method chaining between base and child classes. So that the following
+ * would work and the method chain would not be interrupted
+ * <br>
+ * {@code new TestHelper(otherHelper).setOverviewUrl("nav url").setRank(1).defaultStep("product url"); }
+ * <br>
+ * @param <T> Basically the type of the child class.
+ */
 public abstract class RankingHelper<T extends RankingHelper<T>> implements Serializable
 {
+	private final boolean isRankingMode;
 	private String overviewUrl;
-	private int rank;
+	private Integer rank;
 
+	public RankingHelper(IQuery query) {
+		this.isRankingMode = ModiHelper.isRankingMode(query);
+	}
+
+	public RankingHelper(RankingHelper other) {
+		this.isRankingMode = other.isRankingMode;
+		this.rank = other.rank;
+		this.overviewUrl = other.overviewUrl;
+	}
+
+	/**
+	 * Should be used instead of {@code this} in all base classes to provide support for chaining between base and child class
+	 * @return the instance of the current class
+	 */
 	protected abstract T self();
 
-	public T setRank(int rank) {
-		this.rank = rank;
+	public T setRank(Integer rank) {
+		if (isRankingMode)
+			this.rank = rank;
 		return self();
 	}
 
 	public T setOverviewUrl(String overviewUrl) {
-		this.overviewUrl = overviewUrl;
+		if (isRankingMode)
+			this.overviewUrl = overviewUrl;
 		return self();
 	}
 
@@ -25,10 +53,6 @@ public abstract class RankingHelper<T extends RankingHelper<T>> implements Seria
 
 	public String getOverviewUrl() {
 		return overviewUrl;
-	}
-
-	public static OfferKey createTableOfferKey(String k) {
-		return "TD_" + ParserUtil.getCleanedOfferKeyName(k);
 	}
 
 }
