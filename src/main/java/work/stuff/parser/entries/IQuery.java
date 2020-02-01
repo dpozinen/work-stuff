d2011423d87bd33b9a3c6465b30d812b7679acde
dpozinen <dpozinen@gmail.com>
2020-02-01 12:24:36 +0200

    nuke

--- src/main/java/work/stuff/parser/entries/IQuery.java
+++
@@ -1,5 +0,0 @@
-package main.java.work.stuff.parser.entries; /* Dariy */
-
-public class IQuery {
-
-}
--- src/main/java/work/stuff/parser/entries/UriWrapper.java
+++
@@ -1,5 +0,0 @@
-package main.java.work.stuff.parser.entries; /* Dariy */
-
-class UriWrapper {
-
-}
